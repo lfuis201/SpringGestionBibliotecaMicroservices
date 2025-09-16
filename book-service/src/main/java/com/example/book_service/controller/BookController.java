40ce9504f70e1777e0bb62c5d99d892605a5d5a3
lfuis201 <lmamania@ulasalle.edu.pe>
2025-09-16 03:12:08 -0500

    feat: add Book crud whit resilence

---
+++ book-service/src/main/java/com/example/book_service/controller/BookController.java
@@ -0,0 +1,45 @@
+package com.example.book_service.controller;
+
+
+
+import com.example.book_service.dto.BookDTO;
+import com.example.book_service.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping
+    public List<BookDTO> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO bookDTO) {
+        return ResponseEntity.ok(bookService.createBook(bookDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookDTO> updateBook(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
+        return ResponseEntity.ok(bookService.updateBook(id, bookDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ book-service/src/main/java/com/example/book_service/service/BookService.java
@@ -0,0 +1,15 @@
+package com.example.book_service.service;
+
+
+import com.example.book_service.dto.BookDTO;
+
+import java.util.List;
+
+public interface BookService {
+    List<BookDTO> getAllBooks();
+    BookDTO getBookById(Long id);
+    BookDTO createBook(BookDTO bookDTO);
+    BookDTO updateBook(Long id, BookDTO bookDTO);
+    void deleteBook(Long id);
+    void deactivateBook(Long id);
+}
